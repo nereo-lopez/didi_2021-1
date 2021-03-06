4cc24acb7542b6b4beafbc4929eb00ef8c830791
Nereo Lopez <nereo.lopez@uabc.edu.mx>
2021-03-05 17:49:24 -0800

    Commit Práctica 3

---
+++ Unity Project/Assets/Codes/Index.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Index : MonoBehaviour
+{
+
+    void Start()
+    {
+        int [] nums = {2,7,11,15};
+        int [] toSum = nums;
+        int target = 9;
+        
+        sumaNums(nums,toSum,target);
+        
+    }
+
+    public int sumaNums(int [] nums, int [] toSum, int target)
+    {
+        int res=0;
+
+        for (int i=0;i<nums.Length;i++)
+        {
+            for(int j=0;j<toSum.Length;j++)
+            {
+                res=nums[i]+toSum[j];
+                if(res==target)
+                {                
+                    Debug.Log("["+i+"]");
+                }
+
+            }
+        }
+
+        return 0;
+        
+    }
+
+    //Time Complexity: 
+}
+
